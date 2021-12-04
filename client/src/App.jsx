9e4cee20c424b2a6ee3be5b48a04abdb88675a76
buivantan01 <buivantan1234566@gmail.com>
2021-12-04 13:53:09 +0700

    Updated Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,13 @@ import { BrowserRouter } from 'react-router-dom';
 import './scss/index.scss';
 
 import Header from './components/Header';
-
+import Footer from './components/Footer';
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <div className="container"></div>
+      <Footer />
     </BrowserRouter>
   );
 }
